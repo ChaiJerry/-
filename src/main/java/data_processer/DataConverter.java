5f106e7e7ed96a29c9ed26485366168cfd7f986a
刘云韬 <22371253@buaa.edu.cn>
2024-07-28 16:15:14 +0800

    feat: added function of getting items and tickets by ticketAttributes
    by extending functions of searchUnit and QuerySystem.java.
    Make use of it by calling singleAttributeFreqQuery() method

--- src/main/java/data_processer/DataConverter.java
+++ src/main/java/data_processer/DataConverter.java
@@ -2,7 +2,11 @@ package data_processer;
 
 import org.apache.spark.sql.*;
 
+import java.io.*;
 import java.util.*;
+
+import static io.IOMonitor.*;
+import static query_system.QuerySystem.*;
 
 public class DataConverter {
     private DataConverter() {
@@ -140,7 +144,57 @@ public class DataConverter {
         return result;
     }
 
+    public boolean strs2txt() throws IOException {
+        List<String> strings = queryTest2();
+        // 指定要写入的文件路径
+        String filePath = "C:\\Users\\mille\\Desktop\\output.txt";
+
+        // 使用try-with-resources语句来自动关闭资源
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            // 遍历List中的每个字符串
+            for (String str : strings) {
+                // 将字符串写入文件，并在每个字符串后添加换行符
+                writer.write(str + System.lineSeparator());
+            }
+
+            // 实际上，在try-with-resources中，当退出try块时，writer会自动被关闭和刷新
+            // 但为了明确性，这里也可以显式调用flush方法（虽然不是必需的）
+            writer.flush();
+
+            System.out.println("Successfully wrote strings to the file.");
+        } catch (IOException e) {
+            // 处理异常
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
     public static int setBitPos2zero(int num, int bitPos) {
         return num & ~(1 << bitPos);
+    }
+
+    public static List<Map.Entry<String, String>> map2List(Map<String, String> map, int type) {
+        switch (type) {
+            case HOTEL:
+                map.remove("HOTEL_NAME");
+                break;
+            case MEAL:
+                map.remove("MEAL_NAME");
+                map.remove("MEAL_CODE");
+                break;
+            case BAGGAGE:
+                break;
+            case INSURANCE:
+                map.remove("INSURANCE_COMPANY");
+                map.remove("INSURANCE_COMPANYCODE");
+                break;
+            case SEAT:
+                break;
+            default:
+                break;
+        }
+
+        return new ArrayList<>(map.entrySet());
     }
 }
--- src/main/java/main_class/Main.java
+++ src/main/java/main_class/Main.java
@@ -11,26 +11,6 @@ import static query_system.QuerySystem.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        List<String> strings = queryTest2();
-        // 指定要写入的文件路径
-        String filePath = "C:\\Users\\mille\\Desktop\\output.txt";
 
-        // 使用try-with-resources语句来自动关闭资源
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            // 遍历List中的每个字符串
-            for (String str : strings) {
-                // 将字符串写入文件，并在每个字符串后添加换行符
-                writer.write(str + System.lineSeparator());
-            }
-
-            // 实际上，在try-with-resources中，当退出try块时，writer会自动被关闭和刷新
-            // 但为了明确性，这里也可以显式调用flush方法（虽然不是必需的）
-            writer.flush();
-
-            System.out.println("Successfully wrote strings to the file.");
-        } catch (IOException e) {
-            // 处理异常
-            e.printStackTrace();
-        }
     }
 }
