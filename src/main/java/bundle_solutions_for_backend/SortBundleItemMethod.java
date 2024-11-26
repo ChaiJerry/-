7287bc4067a5ea01f1e44f0579f3071d4578126b
刘云韬 <22371253@buaa.edu.cn>
2024-11-26 23:41:30 +0800

    feat : 增加保险加入document的功能

--- src/main/java/bundle_solutions_for_backend/SortBundleItemMethod.java
+++ src/main/java/bundle_solutions_for_backend/SortBundleItemMethod.java
@@ -1,12 +1,14 @@
 package bundle_solutions_for_backend;
 
 import bundle_system.memory_query_system.*;
+import org.w3c.dom.*;
 import xml_parser.*;
 
 import java.util.*;
 
 @FunctionalInterface
 public interface SortBundleItemMethod {
-    List<BundleItem> execute(Map<String, BundleItem> ticketInfo
-            , Map<String, List<BundleItem>> bundleItems, RulesStorage rulesStorage);
+    void execute(Map<String, BundleItem> ticketInfo
+            , Map<String, List<BundleItem>> bundleItems
+            , RulesStorage rulesStorage, Element comboWith,Document doc);
 }
