f26b6d65fb0a9ec24914e6f6afb2e3d30fa39572
刘云韬 <22371253@buaa.edu.cn>
2024-11-29 13:30:25 +0800

    fix : 去掉了之前遗留的无用引用

--- src/main/java/bundle_service_for_backend/SortBundleItemMethod.java
+++ src/main/java/bundle_service_for_backend/SortBundleItemMethod.java
@@ -3,7 +3,6 @@ package bundle_service_for_backend;
 import bundle_service_for_backend.xml_parser.*;
 import bundle_system.memory_query_system.*;
 import org.w3c.dom.*;
-import xml_parser.*;
 
 import java.util.*;
 
