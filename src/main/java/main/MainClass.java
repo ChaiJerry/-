ca932802b8af669cdf1258e6f698366ccfe74a1b
刘云韬 <22371253@buaa.edu.cn>
2024-11-29 23:04:44 +0800

    feat : 给pom加了个插件，可以打jar包了
    第二个接口的方法名字修改为submitBundleTask，确定了第一个接口的可用性

--- src/main/java/main/MainClass.java
+++ src/main/java/main/MainClass.java
@@ -1,8 +1,12 @@
-package bundle_system.main_class;
+package main;
 
 import bundle_service_for_backend.*;
-import bundle_system.io.sql.*;
 import bundle_system.memory_query_system.*;
+import bundle_system.api.*;
+import bundle_system.io.*;
+import bundle_system.data_generating_system.*;
+import bundle_system.db_query_system.*;
+import bundle_system.data_processer.*;
 import org.xml.sax.*;
 
 import javax.xml.parsers.*;
@@ -13,8 +17,8 @@ import java.sql.*;
 
 import static bundle_system.io.SharedAttributes.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException, SQLException, ParserConfigurationException, SAXException, XPathExpressionException, TransformerException {
+public class MainClass {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException, XPathExpressionException, TransformerException {
 //        QuickQuery quickQuery = new QuickQuery();
 //        quickQuery.test(MEAL);
         BackendBundleSystem backendBundleSystem = new BackendBundleSystem(4);
